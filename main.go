1e717c11f8ae4a9bb6ae5a68472b8a8120551d5b
Eric Windisch <ewindisch@docker.com>
2014-10-29 16:19:52 +0000

    Update.. make it work

--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import "fmt"
 import "syscall"
+import "io/ioutil"
 
 func main() {
-    r1, _, err1 := syscall.RawSyscall6(syscall.SYS_CLONE, uintptr(syscall.SIGCHLD), 0, 0, 0, 0, 0)
-    if err1 != 0 {
-        //runtime_AfterFork()
-        fmt.Println("Hello child error")
-        fmt.Println(err1)
-        return
-    }
-    if r1 != 0 {
-        fmt.Println("Hello child")
-        return
-    }
-  
+	r1, _, err := syscall.RawSyscall6(syscall.SYS_CLONE, uintptr(syscall.SIGCHLD), 0, 0, 0, 0, 0)
+	if err != 0 {
+		fmt.Println(err)
+	}
 
-    fmt.Println("Hello, 世界")
+	if r1 != 0 {
+		fmt.Println("Hello child")
+
+		syscall.Chroot("/tmp")
+		files, _ := ioutil.ReadDir("/")
+		for _, f := range files {
+			fmt.Println(f.Name())
+		}
+		return
+	}
+
+    	fmt.Println("Hello parent")
+	files, _ := ioutil.ReadDir("/")
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
